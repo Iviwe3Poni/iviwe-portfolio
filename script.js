26e7e73d5d092b236642d95157b95afb4022ccc1
Iviwe3Poni <168042569+Iviwe3Poni@users.noreply.github.com>
2024-11-12 23:01:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listeners to the download links
+document.getElementById('cv-download').addEventListener('click', function() {
+    alert('You are downloading Iviwe’s CV.');
+});
+
+document.getElementById('matric-download').addEventListener('click', function() {
+    alert('You are downloading Iviwe’s Matric Certificate.');
+});
